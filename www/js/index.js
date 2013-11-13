2746ebaceab813598c975c2859ca52d196863bc5
Austin Lazarus <austin.lazarus@gmail.com>
2013-11-13 06:47:35 -0800

    Geo-location event trial

--- www/js/index.js
+++ www/js/index.js
@@ -15,3 +15,36 @@ map.locate({setView: true});
 //##### Adds Marker to map at specified coordinates  ####//
 L.marker([15.304221,-61.384134]).addTo(map)
 .bindPopup("<b>Roseau</b>").openPopup(); //<== Creates a Popup on the marker 
+
+
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + position.timestamp                    + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+	
