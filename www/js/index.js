261a2454619f1230b54dacb4822d0e96b0bdf436
Austin Lazarus <austin.lazarus@gmail.com>
2013-11-13 06:08:52 -0800

    New changes to the DOM and javascript/map code

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,17 @@
-// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('#map').setView([51.505, -0.09], 13);
+//####### Initialize the Map  #######//
+var map = L.map('map',
+                {
+                  center: [15.304221,-61.384134],
+                  animate: true,
+                  zoom:20
+                });
+
+//####### Load and display tile layers on the map #######//
+L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png').addTo(map);
+
+//####### Automatically locates and sets a maximum view #######//
+map.locate({setView: true});
+
+//##### Adds Marker to map at specified coordinates  ####//
+L.marker([15.304221,-61.384134]).addTo(map)
+.bindPopup("<b>Roseau</b>").openPopup();
